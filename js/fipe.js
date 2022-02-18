5b3eabe56ea9bdf5d8cf9e23b12938b065bc1dbc
Helvis Lima <helvis.slima@gmail.com>
2022-02-18 18:19:46 -0300

    Finishing integration and design of informations for showing to user.

--- js/fipe.js
+++ js/fipe.js
@@ -7,38 +7,52 @@ $(document).ready(function() {
 
 function request(type, body, element) {
     $.post("https://veiculos.fipe.org.br/api/veiculos/" + type, body, function( data ) {
-        if(element == "#model" || element == "#year") {
+        if(element == "#model") {
+            $.each(data.Modelos, function(i, p) {
+                $(element).append($('<option value="'+p.Value+'">'+p.Label+'</option>'));
+            });
+        } else if(element == "#brand" || element == "#year"){
             $.each(data, function(i, p) {
                 $(element).append($('<option value="'+p.Value+'">'+p.Label+'</option>'));
             });
-        } else if(element == "#brand"){
-            $.each(data.Modelos, function(i, p) {
-                $("#brand").append($('<option value="'+p.Value+'">'+p.Label+'</option>'));
-            });
-        } 
+        }  else if (element == "#informations") {
+            clearSearch();
+            $("#informations").show();
+            $(".loading").hide();
+            $("#carBrandModel").text(data.Marca + ' - ' + data.Modelo);
+            $("#carInfo").text('Ano: '+data.AnoModelo+' / À '+data.Combustivel+'');
+            $("#carValue").text(data.Valor);
+            $("#searchDate").text('Referência: '+data.MesReferencia+'');
+        }
     });
 }
 
 function loadVehicles() {
     var type = "ConsultarMarcas"
-    var body = {'codigoTabelaReferencia': 282, 'codigoTipoVeiculo': 1};
-    var element = "#model";
+    var body = {
+        'codigoTabelaReferencia': 282, 
+        'codigoTipoVeiculo': 1
+    }
+    var element = "#brand";
     request(type, body, element);
 }
 
-function getVehicleModelInfo() {
-    $("#brand").empty();
-    $("#year").empty();
-    loadVehicles();
-    getVehicleBrandInfo();
+function getVehicleBrandInfo() {
+    $("#model").find('option').not(':first').remove();
+    $("#year").find('option').not(':first').remove();
+    getVehicleModelInfo();
 }
 
-function getVehicleBrandInfo(fromChange = null) {
-    $("#brand").prop("disabled", false);
+function getVehicleModelInfo(fromChange = null) {
+    $("#model").prop("disabled", false);
     var type = "ConsultarModelos"
-    var brand = $("#model").val();
-    var body = {'codigoTabelaReferencia': 282, 'codigoTipoVeiculo': 1, 'codigoMarca': brand};
-    var element = "#brand";
+    var brand = $("#brand").val();
+    var body = {
+        'codigoTabelaReferencia': 282, 
+        'codigoTipoVeiculo': 1, 
+        'codigoMarca': brand
+    };
+    var element = "#model";
     request(type, body, element);
     if(fromChange) {
         getVehicleYearInfo();
@@ -46,11 +60,45 @@ function getVehicleBrandInfo(fromChange = null) {
 }
 
 function getVehicleYearInfo() {
+    $("#year").find('option').not(':first').remove();
     $("#year").prop("disabled", false);
-    var type = "ConsultarAnoModelo"
-    var brand = $("#model").val();
-    var model = $("#brand").val();
-    var body = {'codigoTabelaReferencia': 282, 'codigoTipoVeiculo': 1, 'codigoMarca': brand, 'codigoModelo': model};
+    var type = "ConsultarAnoModelo";
+    var brand = $("#brand").val();
+    var model = $("#model").val();
+    var body = {
+        'codigoTabelaReferencia': 282, 
+        'codigoTipoVeiculo': 1, 
+        'codigoMarca': brand, 
+        'codigoModelo': model
+    };
     var element = "#year";
+    request(type, body, element);
+}
+
+function clearSearch() {
+    $("#carBrandModel").empty()
+    $("#carInfo").empty();
+    $("#carValue").empty()
+    $("#searchDate").empty();
+}
+function getAllParameters() {
+    $(".loading").show();
+    var type = "ConsultarValorComTodosParametros";
+    var year = $("#year").val();
+    var splitYearFuel = year.split("-");
+    var year = splitYearFuel[0];
+    var fuel = splitYearFuel[1];
+    var element = "#informations";
+    var body = {
+        "codigoTabelaReferencia": 282,
+        "codigoMarca": parseInt($("#brand").val()),
+        "codigoModelo": parseInt($("#model").val()),
+        "codigoTipoVeiculo": 1,
+        "anoModelo": parseInt(year),
+        "codigoTipoCombustivel": parseInt(fuel),
+        "tipoVeiculo": "carro",
+        "modeloCodigoExterno": null,
+        "tipoConsulta": "tradicional"
+    };
     request(type, body, element);
 }
--- views/fipe.php
+++ views/fipe.php
@@ -5,7 +5,7 @@
     <script src="<?=plugin_dir_url(__DIR__)?>js/fipe.js"></script>
     <link href=<?=plugin_dir_url(__DIR__)?>css/select2.min.css" rel="stylesheet" />
     <script src="<?=plugin_dir_url(__DIR__)?>js/select2.min.js" defer></script>
-
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.3.1/css/all.min.css" rel="stylesheet">
     <style>
         .select2-selection__rendered {
             font-size: 14px !important
@@ -17,32 +17,50 @@
 </head>
 
 <div id="component"> 
-    
     <h3>CONSULTA TABELA FIPE</h3>
-
     <div class="content">
         <label>Marca do veiculo</label>
-        <select name="model" onChange="getVehicleModelInfo()" id="model"> 
+        <select name="brand" onChange="getVehicleBrandInfo()" id="brand"> 
             <option>digite ou selecione uma marca...</option>
         </select>
     </div>
-    
+
     <div class="content">
         <label>Modelo do veículo</label>
-        <select name="brand" onChange="getVehicleBrandInfo(true)" disabled id="brand">
+        <select name="model" onChange="getVehicleModelInfo(true)" disabled id="model">
             <option>digite ou selecione um modelo...</option>
         </select>
     </div>
 
     <div class="content">
         <label>Ano do modelo do veículo</label>
-        <select name="year" onChange="getVehicleYearInfo()" disabled id="year">  
+        <select name="year" disabled id="year">  
             <option>digite ou selecione o ano...</option>
         </select>
     </div>
 
-    <button class="send-button" onClick="">Obter informações</button>
-
-    <div id="informations"></div>
-
-</div>  
+    <button class="send-button" onClick="getAllParameters()">Obter informações</button>
+    <div class="loading" style="display:none"><i class="fas fa-spinner fa-spin"></i> Buscando dados ...</div>
+    <div id="informations" style="display: none">
+        <div class="card-body">
+            <div class="row">
+            <div class="col">
+                <h5 class="card-title text-uppercase text-muted mb-0" id="carBrandModel"></h5>
+                <p class="mt-3 mb-0 text-muted text-sm">
+                    <span class="text-nowrap" id="carInfo"></span>
+                </p>
+                <span class="h2 font-weight-bold mb-0" id="carValue"></span>
+            </div>
+            <div class="col-auto">
+                <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
+                <i class="fas fa-chart-bar"></i>
+                </div>
+            </div>
+            </div>
+            <p class="mt-3 mb-0 text-muted text-sm">
+                <span class="text-nowrap" id="searchDate"></span>
+            </p>
+        </div>
+    </div>
+</div>
+</body>
